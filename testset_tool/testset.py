dd64aa27a1985c1713393a19c8ea69804666b698
Jan Hutar <jhutar@redhat.com>
2021-03-06 23:25:40 +0100

    testset_tool/testset.py: Lazy loading and questions to ask count is in area now

--- testset_tool/testset.py
+++ testset_tool/testset.py
@@ -11,23 +11,61 @@ class Question(object):
 
     def __init__(self, filename):
         logging.info(f"Creating question from {filename}")
-        self.filename = filename
+        self._filename = filename
 
-        with open(filename, 'r') as fp:
+        self._type = None
+        self._timeout = None
+        self._question = None
+        self._answers = None
+        self._correct = None
+
+    def load_meta(self):
+        with open(self._filename, 'r') as fp:
             metadata = yaml.load(fp, Loader=yaml.Loader)
-            self.type = metadata['type']
-            self.timeout = int(metadata['timeout'])
-            self.question = metadata['question']
-            self.answers = metadata['answers']
-            self.correct = metadata['correct']
+            self._type = metadata['type']
+            self._timeout = int(metadata['timeout'])
+            self._question = metadata['question']
+            self._answers = metadata['answers']
+            self._correct = metadata['correct']
 
     def __repr__(self):
         return f"<Question({self.filename}, {self.type}, {self.timeout})>"
 
+    @property
+    def type(self):
+        if self._type is None:
+            self.load_meta()
+        return self._type
+
+    @property
+    def timeout(self):
+        if self._timeout is None:
+            self.load_meta()
+        return self._timeout
+
+    @property
+    def question(self):
+        if self._question is None:
+            self.load_meta()
+        return self._question
+
+    @property
+    def answers(self):
+        if self._answers is None:
+            self.load_meta()
+        return self._answers
+
+    @property
+    def correct(self):
+        if self._correct is None:
+            self.load_meta()
+        return self._correct
+
     def lint(self):
-        logging.debug(f"Linting question from {self.filename}")
+        logging.debug(f"Linting question from {self._filename}")
         assert self.type == 'abc'
         assert self.timeout > 0
+        assert isinstance(self.question, str) and len(self.question) > 0
         assert isinstance(self.answers, dict)
         assert self.correct in self.answers
 
@@ -44,27 +82,52 @@ class Question(object):
 class Area(object):
     """Represent testset area as an object."""
 
-    def __init__(self, dirname, questions_to_ask):
+    def __init__(self, dirname):
         logging.info(f"Creating area from {dirname}")
-        self.dirname = dirname
-        self.name = os.path.basename(dirname)
-        self.questions_to_ask = int(questions_to_ask)
+        self._dirname = dirname
+        self._name = os.path.basename(dirname)
 
-        self.questions = []
-        for item in os.listdir(dirname):
-            if os.path.isfile(os.path.join(dirname, item)) \
-               and (item.endswith('.yaml') or item.endswith('.yml')):
-                question = Question(os.path.join(dirname, item))
-                self.questions.append(question)
+        self._questions_to_ask = None
+        self._questions = None
+
+    def load_meta(self):
+        with open(os.path.join(self._dirname, 'metadata.yaml'), 'r') as fp:
+            metadata = yaml.load(fp, Loader=yaml.Loader)
+            self._questions_to_ask = int(metadata['questions_to_ask'])
+
+    def load_questions(self):
+        self._questions = []
+        for item in os.listdir(self._dirname):
+            if os.path.isfile(os.path.join(self._dirname, item)) \
+               and (item.endswith('.yaml') or item.endswith('.yml')) \
+               and item != 'metadata.yaml':
+                question = Question(os.path.join(self._dirname, item))
+                self._questions.append(question)
 
     def __gt__(self, other):
-        return self.dirname > other.dirname
+        return self._dirname > other._dirname
 
     def __repr__(self):
         return f"<Area({self.basename}, {self.questions_to_ask})>"
 
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def questions_to_ask(self):
+        if self._questions_to_ask is None:
+            self.load_meta()
+        return self._questions_to_ask
+
+    @property
+    def questions(self):
+        if self._questions is None:
+            self.load_questions()
+        return self._questions
+
     def lint(self):
-        logging.debug(f"Linting area from {self.dirname}")
+        logging.debug(f"Linting area from {self._dirname}")
         assert len(self.name) > 0
         assert self.questions_to_ask > 0
         assert self.questions_to_ask <= len(self.questions)
@@ -85,29 +148,50 @@ class TestSet(object):
 
     def __init__(self, dirname):
         logging.info(f"Creating testset from {dirname}")
-        self.dirname = dirname
+        self._dirname = dirname
 
-        with open(os.path.join(dirname, 'metadata.yaml'), 'r') as fp:
-            metadata = yaml.load(fp, Loader=yaml.Loader)
-            self.name = metadata['name']
-            self.version = int(metadata['version'])
-            self.questions_to_ask = metadata['questions_to_ask']
-
-        self.areas = []
-        for item in os.listdir(dirname):
-            if os.path.isdir(os.path.join(dirname, item)) and item in self.questions_to_ask:
-                area = Area(os.path.join(dirname, item), self.questions_to_ask[item])
-                self.areas.append(area)
-        self.areas.sort()
+        self._name = None
+        self._version = None
+        self._areas = None
 
     def __repr__(self):
-        return f"<TestSet({self.dirname}, {self.version})>"
+        return f"<TestSet({self._dirname}, {self.version})>"
+
+    @property
+    def name(self):
+        if self._name is None:
+            self.load_meta()
+        return self._name
+
+    @property
+    def version(self):
+        if self._version is None:
+            self.load_meta()
+        return self._version
+
+    @property
+    def areas(self):
+        if self._areas is None:
+            self.load_areas()
+        return self._areas
+
+    def load_meta(self):
+        with open(os.path.join(self._dirname, 'metadata.yaml'), 'r') as fp:
+            metadata = yaml.load(fp, Loader=yaml.Loader)
+            self._name = metadata['name']
+            self._version = int(metadata['version'])
+
+    def load_areas(self):
+        self._areas = []
+        for item in os.listdir(self._dirname):
+            if os.path.exists(os.path.join(self._dirname, item, 'metadata.yaml')):
+                area = Area(os.path.join(self._dirname, item))
+                self._areas.append(area)
+        self._areas.sort()
 
     def lint(self):
-        logging.debug(f"Linting testset from {self.dirname}")
+        logging.debug(f"Linting testset from {self._dirname}")
         assert self.version > 0
-        assert len(self.questions_to_ask) > 0
-        assert len(self.questions_to_ask) == len(self.areas)
 
         for area in self.areas:
             area.lint()
