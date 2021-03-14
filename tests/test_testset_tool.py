f84512a48981212d9c0cdc6ff66da5f306404dd0
Jan Hutar <jhutar@redhat.com>
2021-03-14 23:12:12 +0100

    tests: Some very basic tests

--- tests/test_testset_tool.py
+++ tests/test_testset_tool.py
@@ -3,9 +3,11 @@
 """Tests for `testset_tool` package."""
 
 
+import contextlib
+import io
 import unittest
 
-from testset_tool import testset_tool
+from testset_tool import testset
 
 
 class TestTestset_tool(unittest.TestCase):
@@ -17,5 +19,21 @@ class TestTestset_tool(unittest.TestCase):
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
-    def test_000_something(self):
-        """Test something."""
+    def test_ok_load(self):
+        """Test testset load."""
+        ts = testset.TestSet("tests/fixtures/ok/")
+
+    def test_ok_show(self):
+        """Test testset show."""
+        ts = testset.TestSet("tests/fixtures/ok/")
+        f = io.StringIO()
+        with contextlib.redirect_stdout(f):
+            ts.show()
+        self.assertTrue("Name: Demo testset for SelfTest" in f.getvalue())
+        self.assertTrue("Area: 01_first" in f.getvalue())
+        self.assertTrue("Question: <p>From following possibilities choose <strong>first</strong> possibility.</p>" in f.getvalue())
+
+    def test_ok_lint(self):
+        """Test testset lint."""
+        ts = testset.TestSet("tests/fixtures/ok/")
+        ts.lint()
